ff1b103c6aff650ac9006f9e81fa0f1eac8ead8b
Francisco jose bello salcedo <developer@MacBook-Pro-de-Francisco.local>
2023-02-22 00:22:37 -0500

    add more img

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded",()=>{
+  /*
+   get some variables from the document
+  */
   const btnCard=document.querySelector(".card_btn");
   const faceFrontCard=document.querySelector(".face_front");
   const faceBackCard=document.querySelector(".face_back");
+
   btnCard.addEventListener("click",()=>{
-    faceFrontCard.classList.toggle("turn_around_face_front");
-    faceBackCard.classList.toggle("turn_around_face_back");
+    faceFrontCard.classList.add("turn_around_face_front");
+    faceBackCard.classList.add("turn_around_face_back");
     console.log("hola")
   });
 
